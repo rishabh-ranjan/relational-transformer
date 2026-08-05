9878365a815bfd49a0c8d6930cfe50be86f0b68e
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2026-08-04 17:58:35 -0700

    clean up

--- expts/dbinfer/rel2tab/predictors/__init__.py
+++
@@ -1,6 +0,0 @@
-from rel2tab.predictors.tabicl_batched_predictor import (
-    TabICLBatchedPredictor,
-    TabICLBatchedPredictorConfig,
-)
-
-__all__ = ["TabICLBatchedPredictor", "TabICLBatchedPredictorConfig"]
