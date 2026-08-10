e994e95351db4663e3a730c421e0938dabb30b99
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2026-08-10 01:35:03 -0700

    fine_tune: move the rtp study-outcome job to a b200

--- expts/fine_tune/submit_rtp.py
+++ expts/fine_tune/submit_rtp.py
@@ -9,15 +9,15 @@ from roach.slurm import Resources, submit
 HERE = Path(__file__).parent
 
 TASKS = (
-    ("rel-avito", "ad-ctr"),
+    # ("rel-avito", "ad-ctr"),
     # ("rel-avito", "user-clicks"),
     # ("rel-avito", "user-visits"),
     # ("rel-f1", "driver-dnf"),
     # ("rel-f1", "driver-position"),
     # ("rel-f1", "driver-top3"),
-    ("rel-event", "user-attendance"),
+    # ("rel-event", "user-attendance"),
     # ("rel-event", "user-ignore"),
-    ("rel-event", "user-repeat"),
+    # ("rel-event", "user-repeat"),
     # ("rel-trial", "site-success"),
     # ("rel-trial", "study-adverse"),
     ("rel-trial", "study-outcome"),
