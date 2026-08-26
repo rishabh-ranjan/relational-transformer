f30994ed54f8fc3847ddd2c73470f5e4f9669f98
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2026-08-26 09:54:03 -0700

    pretrain_abl: mix-forecast next in hold 447124

--- expts/repaper/pretrain_abl/submit.py
+++ expts/repaper/pretrain_abl/submit.py
@@ -9,11 +9,11 @@ submit(
     args=args()
     # | dict(run_name="mask0", mask_prob_max=0.0)
     # | dict(run_name="mask25", mask_prob_max=0.25)
-    | dict(run_name="mask75", mask_prob_max=0.75)
-    # | dict(
-    #     run_name="mix-forecast",
-    #     db_task_list="expts/repaper/pretrain_abl/cutoff-forecast.json",
-    # )
+    # | dict(run_name="mask75", mask_prob_max=0.75)
+    | dict(
+        run_name="mix-forecast",
+        db_task_list="expts/repaper/pretrain_abl/cutoff-forecast.json",
+    )
     # | dict(
     #     run_name="mix-autocomplete",
     #     db_task_list="expts/repaper/pretrain_abl/cutoff-autocomplete.json",
